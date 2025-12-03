8f8a614c19d5ce68e8a625b74f0f1fe457216815
Tiwari Shubham <Shubham.injoria@gmail.com>
2025-12-03 23:06:37 +0530

    second commit

--- 01_basics/01_basic.ts
+++ 01_basics/01_basic.ts
@@ -3,3 +3,4 @@ function greeting(username : string) : string {
 }
 
 console.log(greeting('Shubham'))
+// console.log(greeting(47)) using other data types get error in the code this is best way to understand error in code 
